421d05201a7c08f0b041aa48ddab0dcf5f371288
Jorge <george.gacitua@gmail.com>
2019-12-30 15:13:56 -0300

    a

--- darkflow/net/yolov2/train.py
+++ darkflow/net/yolov2/train.py
@@ -130,7 +130,7 @@ def loss(self, net_out):
     self.fetch += [_probs, confs, conid, cooid, proid]
     confs = tf.reshape(confs, [-1, H*W, B])
     true = tf.concat([_coord, tf.expand_dims(confs, 3), _probs ], 3)
-    wght = tf.concat([cooid, tf.expand_dims(conid, 4), proid ], 3)
+    wght = tf.concat([cooid, tf.expand_dims(conid, 3), proid ], 3)
 
     print('Building {} loss'.format(m['model']))
     loss = tf.pow(adjusted_net_out - true, 2)
